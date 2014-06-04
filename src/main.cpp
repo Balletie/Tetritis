2ec8e079534714fde59545940edcff2d75a661e1
Balletie <skip_meesie@hotmail.com>
2014-06-05 00:53:11 +0200

    call srand at start for different random blocks each game

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "window.h"
+#include <stdlib.h>
 
 int main() {
+	srand(time(NULL));
 	if (!window::open()) return EXIT_FAILURE;
 	if (!window::loop()) return EXIT_FAILURE;
 	return EXIT_SUCCESS;
